6a371bedc50feaf4b84e0f2a5474f5c29bae6a02
JFCeron <juanfceron@gmail.com>
2021-08-18 18:02:34 -0500

    Correct flag initialization in generation test

--- best_of_n/test_generate_N.py
+++ best_of_n/test_generate_N.py
@@ -53,6 +53,7 @@ def _define_flags():
         default=None,
         help="Temporary dir for internal use of BestOfNGenerator"
     )
+    return flags.FLAGS
 
 def main(_):
     FLAGS = _define_flags()
